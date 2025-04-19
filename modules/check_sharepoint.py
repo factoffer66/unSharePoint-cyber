e1ad0db1dcc729bbc60bf3e9cdf5c5767f3678f1
factoffer66 <57645915+factoffer66@users.noreply.github.com>
2025-04-19 23:50:20 +0300

    potential fixees

--- modules/check_sharepoint.py
+++ modules/check_sharepoint.py
@@ -1,3 +1,4 @@
+# modules/check_sharepoint.py
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -21,7 +22,6 @@ class Sharepoint:
     request = None
 
     def __new__(cls):
-        # Creating Logger as a Singleton and init the colored output
         if cls._instance is None:
             cls._instance = super(Sharepoint, cls).__new__(cls)
             cls._instance.init_sharepoint()
@@ -36,59 +36,78 @@ class Sharepoint:
         self.logger.debug("check_availability")
         try:
             r = self.request.request_get(self.target.url)
-
             if r.status_code != 200:
-                self.logger.warning("Please take attention, the target answered with %i code." % r.status_code)
-
+                self.logger.warning(
+                    "Please take attention, the target answered with %i code." % r.status_code
+                )
         except Exception:
             raise ValueError("Unhandled error on check_availability().")
-
         return r
 
     def check_iis_target(self, headers):
         self.logger.debug("check_iis_target")
-        # Detection by header parse
         detected_version_iis = detect_server_by_headers(headers)
         if detected_version_iis:
             self.target.server = detected_version_iis
-            self.logger.debug("Server version Detected (headers parse): {}".format(detected_version_iis))
+            self.logger.debug(
+                "Server version Detected (headers parse): {}".format(detected_version_iis)
+            )
 
-        # detect the technology if present
         detected_tec = detect_tec_by_headers(headers)
         if detected_tec:
             self.target.technology = detected_tec
-            self.logger.debug("Technology detected (headers parse): {}".format(detected_tec))
+            self.logger.debug(
+                "Technology detected (headers parse): {}".format(detected_tec)
+            )
 
     def check_share_point(self, headers):
         self.logger.debug("check_share_point")
+
+        # try headers first
         detected_version_share_point = self.detect_sharepoint_by_headers(headers)
         if detected_version_share_point:
-            self.logger.debug("SharePoint version Detected (headers parse): {}".format(detected_version_share_point))
+            self.logger.debug(
+                "SharePoint version Detected (headers parse): {}".format(
+                    detected_version_share_point
+                )
+            )
         else:
+            # fallback to service file
             detected_version_share_point = self.detect_sharepoint_by_servicefile()
             if detected_version_share_point:
-                self.logger.debug("SharePoint version Detected (service.cnf parse): {}".format(detected_version_share_point))
+                self.logger.debug(
+                    "SharePoint version Detected (service.cnf parse): {}".format(
+                        detected_version_share_point
+                    )
+                )
 
         if detected_version_share_point:
             self.get_version(detected_version_share_point)
         else:
-            self.logger.error("Unable to detect SharePoint version; get_version will not be executed.")
+            self.logger.error(
+                "Unable to detect SharePoint version; get_version will not be executed."
+            )
             # ensure .sharepoint is always a dict so to_string() won't crash
             self.target.sharepoint = {
-            "version": "Unknown",
-            "date": None
+                "version": "Unknown",
+                "date": None
             }
 
     def detect_sharepoint_by_headers(self, headers):
         self.logger.debug("detect_sharepoint_by_headers")
-        useful_headers = [header for header in headers if "sharepoint" in header.lower()]
+        useful_headers = [
+            header for header in headers if "sharepoint" in header.lower()
+        ]
         if "MicrosoftSharePointTeamServices" in headers.keys():
-            version = headers["MicrosoftSharePointTeamServices"].split(";")[0].split(":")[0]
+            version = headers["MicrosoftSharePointTeamServices"] \
+                        .split(";")[0] \
+                        .split(":")[0]
             return version
-        elif len(useful_headers) > 0:
+        elif useful_headers:
             self.logger.warning(
-                "Header %s was found, it may not bring the exact version." % headers[useful_headers[0]].replace("\r\n",
-                                                                                                                ""))
+                "Header %s was found, it may not bring the exact version." %
+                headers[useful_headers[0]].replace("\r\n", "")
+            )
         return None
 
     def detect_sharepoint_by_servicefile(self):
@@ -96,82 +115,53 @@ class Sharepoint:
         version = None
         for service_url in constants.SHAREPOINT_SERVICE_URLS:
             try:
-
-                # request the url and save the data into the session variable
                 r = self.request.request_get(self.target.url + service_url)
-
                 if r.status_code == 200:
                     if "vti_extenderversion" in r.text:
-                        version = r.text.split("vti_extenderversion:SR|")[1].replace("\n", "")
+                        version = r.text.split("vti_extenderversion:SR|")[1].strip()
                         break
                     if "vti_buildversion" in r.text:
-                        version = r.text.split("vti_extenderversion:SR|")[1].replace("\n", "")
+                        version = r.text.split("vti_extenderversion:SR|")[1].strip()
                         break
-
             except Exception:
                 raise ValueError("Unhandled error detect_sharepoint_by_servicefile().")
-
         return version
 
-    def get_version(self,patch_number):
+    def get_version(self, patch_number):
+        # patch_number guaranteed non-None here
+        tokens = patch_number.split(".")
+        major = int(tokens[0])
+        minor = int(tokens[1])
+        build = int(tokens[3])
+        lookup_version = f"{major}.{minor}.{build}"
 
-
-        _patch_tokens = patch_number.split(".")
-        _major_version = int(_patch_tokens[0])
-        _minor_version = int(_patch_tokens[1])
-        _build_version = int(_patch_tokens[3])
-        _patch_number = "%s.%s.%s" % (_major_version,_minor_version,_build_version)
-        _last_version = ""
-        _last_date = ""
-        db_version = {
-            "version": "",
-            "date": ""
-        }
-
-
-
-        with open('%s%sversions.csv' % (constants.DATABASE_FOLDER, os.path.sep),"r") as file_versions:
-
+        db_version = {"version": "", "date": ""}
+        csv_path = os.path.join(constants.DATABASE_FOLDER, "versions.csv")
+        with open(csv_path, "r") as file_versions:
             for line in file_versions:
-
-                _line_tokens = line.split(",")
-                _checked_version = _line_tokens[0].split(".")
-                _checked_version_date = _line_tokens[2]
-
-                #### if there are not enough tokens, we skip it #######
-
-                if(len(_checked_version)<4):
+                parts = line.strip().split(",")
+                version_parts = parts[0].split(".")
+                date_str = parts[2]
+                if len(version_parts) < 4:
                     continue
 
-                #### Extract major, minor and build version #######
+                v_major, v_minor, v_build = map(int, version_parts[:3])
+                v_date = (
+                    datetime.strptime(date_str, "%Y %B %d")
+                    if date_str != "N/A"
+                    else None
+                )
+                formatted = f"{v_major}.{v_minor}.{v_build}"
 
-                _checked_version_major = int(_checked_version[0])
-                _checked_version_minor = int(_checked_version[1])
-                _checked_version_build = int(_checked_version[2])
-
-                #### reformat checked version date #######
-
-                if(_checked_version_date!="N/A"):
-                    _checked_version_date = datetime.strptime(_checked_version_date, "%Y %B %d")
-
-                #### reformat checked version structure #######
-
-                _checked_version = "%s.%s.%s" % (_checked_version_major,_checked_version_minor,_checked_version_build)
-
-                #### check if we found a version #######
-
-                if(_checked_version == _patch_number):
-                    db_version["version"] = _checked_version
-                    db_version["date"] = _checked_version_date
+                if formatted == lookup_version:
+                    db_version["version"] = formatted
+                    db_version["date"] = v_date
                     self.logger.info("Version details found.")
                     break
 
-                #### if not a suitable version is found, we return the closer one (since they are ordered) #######
-
-                if((_major_version > _checked_version_major) or (_major_version == _checked_version_major and _build_version > _checked_version_build)):
-
-                    db_version["version"] = _checked_version
-                    db_version["date"] = _checked_version_date
+                if (major > v_major) or (major == v_major and build > v_build):
+                    db_version["version"] = formatted
+                    db_version["date"] = v_date
                     self.logger.info("Version could not be found, performing best guess.")
                     break
 
@@ -179,15 +169,8 @@ class Sharepoint:
 
 
 def detect_server_by_headers(headers):
-    if "Server" in headers.keys():
-        return headers["Server"]
-    return None
+    return headers.get("Server", None)
 
 
 def detect_tec_by_headers(headers):
-    if "X-Powered-By" in headers.keys():
-        return headers["X-Powered-By"]
-    return None
-
-
-
+    return headers.get("X-Powered-By", None)
--- unsharepoint.py
+++ unsharepoint.py
@@ -1,10 +1,6 @@
+# unsharepoint.py
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-"""
-Copyright (C) 2020 CYS4 Srl
-See the file 'LICENSE' for copying permission
-"""
 
 import sys
 import urllib3
@@ -22,84 +18,57 @@ from modules.cmdline import gen_cli_args
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
 def main():
-    # print banner
     print_banner()
-
-    # init logger
     logger_instance = Logger()
     logger = logger_instance.get_logger()
 
     try:
-
-        # init options and parse the arguments
         args = gen_cli_args()
-
-        # set the verbose level if flagged
         if args.verbose:
             logger_instance.enable_debug()
 
-        # init request and their auth if needed
         Request(args.domain, args.username, args.password)
-
-        # init target
         tg = Target(args.url)
 
-        # check if we need to update the database an verify the installation
         db = Database()
         if args.update:
             db.update()
-
         db.verify_installation()
 
-        # check if the target is online
         sh = Sharepoint()
-
-        # verify if the target is online or not
         response_obj = sh.check_availability()
-
-        # check the headers in order to look into the technology and the server
         sh.check_iis_target(response_obj.headers)
-
-        # check if we have a sharepoint or something else
         sh.check_share_point(response_obj.headers)
 
-        # check cve based on the build number
-        if tg.sharepoint:
+        # only run CVE check if we have a real numeric version
+        sp = tg.sharepoint or {}
+        ver = sp.get("version", "")
+        if ver and ver.lower() != "unknown":
             check_cve = CheckCVE()
             check_cve.get_cve()
+        else:
+            logger.info("Skipping CVE check: SharePoint version is unknown.")
 
-        # verify if we have to to other checks
         if args.type != 'i':
-
-            if args.type == 'a' or args.type == 'ad':
-                # Api scan, checking SOAP stuff
-                detailed = args.type == 'ad'
+            if args.type in ('a', 'ad'):
                 soap_api = CheckSoapApi()
-                soap_api.check_soap_api(detailed)
-
+                soap_api.check_soap_api(detailed=(args.type == 'ad'))
 
         if args.bruteforce:
-            bt = BruteForce(args.domain, args.username_file, args.password_file)
-            bt.bruteforce()
-
+            BruteForce(args.domain, args.username_file, args.password_file).bruteforce()
         if args.enum_users:
-            ue = UserEnum()
-            ue.user_enumeration()
+            UserEnum().user_enumeration()
 
         logger.info("Scan completed!")
         logger.info("Results:")
         logger.info(tg.to_string())
-
-
 
     except (KeyboardInterrupt, SystemExit):
         sys.exit(2)
     except Exception as e:
         logger.error(e)
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
